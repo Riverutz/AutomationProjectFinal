b237dcbe9441a968474d7da84859c07f597fcb94
Riverutz <alexvlasceanoo@gmail.com>
2024-08-15 11:49:30 +0300

    Added headless argument in Environment class

--- src/test/java/environmentPreparation/Environment.java
+++ src/test/java/environmentPreparation/Environment.java
@@ -4,6 +4,7 @@ import loggerUtility.LogUtil;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -15,7 +16,9 @@ public class Environment {
 
     @BeforeMethod
     public void prepareEnvironment() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
         driver.get("https://www.flax.ro/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
